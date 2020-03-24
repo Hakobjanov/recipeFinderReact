899fb7631c5e42b1ca20bc21951bd6d7eaced97d
vahan akopjanian <vahan.bclub@gmail.com>
2020-03-25 00:11:24 +0300

    Updates

---
+++ precache-manifest.e37af81596cc2797f25abc1a3f0e6457.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "546e178cd33b5ad09c07f0457065ca34",
+    "url": "/recipeFinderReact/index.html"
+  },
+  {
+    "revision": "98571b5adae8765a8a17",
+    "url": "/recipeFinderReact/static/css/main.1a586c5a.chunk.css"
+  },
+  {
+    "revision": "6e6399b281eb67a792f7",
+    "url": "/recipeFinderReact/static/js/2.0a6a514c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipeFinderReact/static/js/2.0a6a514c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98571b5adae8765a8a17",
+    "url": "/recipeFinderReact/static/js/main.e52f8e4f.chunk.js"
+  },
+  {
+    "revision": "2a67e64fb55ddcd685f4",
+    "url": "/recipeFinderReact/static/js/runtime-main.93e1cd4d.js"
+  }
+]);
